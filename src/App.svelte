786b95aa7468e6c5274c5319833b1122f1c2e82c
tiarama <c92macleod@gmail.com>
2021-07-13 15:51:51 +0100

    pizza

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+  import Box from './Box.svelte'
+  let score = 0
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+  <Box bind:score={score} />
+  <h2>Score: {score}</h2>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 400;
+	}
+  h2 {
+    position: absolute;
+    width: 100%;
+    bottom: 10%;
+  }
+</style>
---
+++ src/Box.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let score
+
+  let interval;
+  let isIntervalRunning = false
+
+  let topPosition = 40
+  let leftPosition = 40
+
+  function moveCube() {
+  topPosition = Math.min(Math.random()*100, 90)
+  leftPosition = Math.min(Math.random()*100, 90)
+  }
+
+  function mouseLeave() {
+    if (isIntervalRunning) {
+    alert('YOU GOT CAUGHT BY ANTONIO')
+    clearInterval(interval)
+    isIntervalRunning = false
+    topPosition = 40
+    leftPosition = 40
+    }
+  }
+
+  function myInterval() {
+    if (!isIntervalRunning) {
+    score = 0
+    interval = setInterval(() => {score++; moveCube()}, 1000)
+    isIntervalRunning = true
+    }
+  }
+
+</script>
+
+<!-- {#if !isIntervalRunning} -->
+<img class='box' 
+on:mouseleave={mouseLeave} 
+on:click={myInterval} 
+style='top: {topPosition}vh; left: {leftPosition}vw; cursor: url("smallpineapple.png"), auto;' 
+src="pizza.png" 
+alt="Antonio's nightmare"/>
+  <!-- <p>{topPosition}</p> -->
+<!-- {/if} -->
+
+<style>
+  img {
+    
+    position: absolute;
+    height: 200px;
+    width: 200px;
+    top: 40vh;
+    left: 40vw;
+    transition: top linear 2s, left linear 2s; 
+    border-radius: 100%;
+  }
+</style>
